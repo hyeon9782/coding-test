4ede973c040049f9b0688ec183b679eb5dca9155
hyeon9782 <hyeon9782@gmail.com>
2023-06-16 20:58:56 +0900

    feat: 프로그래머스 코딩테스트 3문제 풀이

--- programmers/Lv.2/01 ~ 10/002.js
+++ programmers/Lv.2/01 ~ 10/002.js
@@ -1,1 +1,16 @@
 // 최솟값 만들기
+function solution(A, B) {
+  let answer = 0;
+
+  // A와 B를 각각 곱해서 총합을 구할 때 최소 값을 만들어야 한다.
+  // A에서 제일 작은 값을 B에서 가장 큰 값으로 곱하면 될듯?
+
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => b - a);
+
+  for (let i = 0; i < A.length; i++) {
+    answer += A[i] * B[i];
+  }
+
+  return answer;
+}
--- programmers/Lv.2/01 ~ 10/010.js
+++ programmers/Lv.2/01 ~ 10/010.js
@@ -1,1 +1,24 @@
 // 영어 끝말잇기
+function solution(n, words) {
+  const usedWords = new Set();
+  let prevWord = words[0];
+
+  for (let i = 1; i < words.length; i++) {
+    const currentWord = words[i];
+
+    if (
+      usedWords.has(currentWord) ||
+      prevWord[prevWord.length - 1] !== currentWord[0]
+    ) {
+      const playerNumber = (i % n) + 1;
+      const turn = Math.ceil((i + 1) / n);
+
+      return [playerNumber, turn];
+    }
+
+    usedWords.add(prevWord);
+    prevWord = currentWord;
+  }
+
+  return [0, 0];
+}
